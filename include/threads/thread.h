807dbfaaa6aaaa7a0edc676df3f8f9b05ac956db
KimGyeongyeon <kgy556@kaist.ac.kr>
2021-04-01 21:23:52 +0900

    1st trial

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -27,6 +27,9 @@ typedef int tid_t;
 #define PRI_MIN 0                       /* Lowest priority. */
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
+#define NICE_DEFAULT 0					/* default nice value*/
+#define RECENT_CPU_DEFAULT 0
+#define LOAD_AVG_DEFAULT 0
 
 /* A kernel thread or user process.
  *
@@ -95,6 +98,26 @@ struct thread {
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem;              /* List element. */
 
+	int64_t wakeup_tick; // store the tick to wake up!!
+
+	/*
+	 *  for priority donation
+	 */	
+	// store priority before donation
+	int pre_priority;
+	// lock that the thread want
+	struct lock *waiting_lock;
+	// store donators
+	struct list donations_list;
+	// in donation list
+	struct list_elem donation_elem;
+
+	/*
+	 *   for MLFQ
+	 */
+	int32_t nice;
+	int32_t recent_cpu;
+
 #ifdef USERPROG
 	/* Owned by userprog/process.c. */
 	uint64_t *pml4;                     /* Page map level 4 */
@@ -142,5 +165,39 @@ int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
 void do_iret (struct intr_frame *tf);
+
+
+/*
+ *  functions we added            
+ */
+
+// for alarm clock
+void update_next_wakeup_tick(int64_t new_ticks);
+int64_t get_next_wakeup_tick(void);
+void go_sleep_thread(int64_t ticks);
+void wakeup_thread(int64_t ticks);
+
+// for priority schedule
+bool compare_p_prior_to_q(const struct list_elem *p, const struct list_elem *q, void *aux UNUSED);
+void yield_according_to_priority (void);
+
+// for priority donation
+void donate_priority (void); 
+void update_priority(void); 
+void remove_donation_related_to_lock(struct lock *lock);
+
+// for mlvqs
+int thread_get_nice (void);
+void thread_set_nice (int nice);
+int32_t thread_get_recent_cpu (void);
+int32_t thread_get_load_avg (void);
+
+void mlfqs_update_priority (struct thread *t); //when mlfqs, update priority
+void mlfqs_update_recent_cpu (struct thread *t); //when mlfqs, update recent cpu
+void mlfqs_update_load_avg(void); //when mlfqs, update load avg
+void mlfqs_recent_cpu_increment(void); //increase recent cpu for every tick
+void mlfqs_update_all_recent_cpu(void); //update recent_cpu of every thread
+void mlfqs_update_all_priority(void); //update priority of every thread
+
 
 #endif /* threads/thread.h */
